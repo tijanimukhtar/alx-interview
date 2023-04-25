cb75417d4be08a756ae4982ac90155e20cb7df7b
Tijani Mukhtar Akande <58123024+tijanimukhtar@users.noreply.github.com>
2023-04-25 16:37:45 +0100

    Adds corrections to  0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,91 +1,64 @@
 #!/usr/bin/python3
-'''N Queens Challenge'''
+""" Solve N Queens problem """
 
 import sys
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+try:
+    n = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+if (n < 4):
+    print("N must be at least 4")
+    sys.exit(1)
 
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
 
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
+def n_queens(num):
+    """ prints every possible solution """
+    result = []
 
-    solutions = []
-    placed_queens = []  # coordinates format [row, column]
-    stop = False
-    r = 0
-    c = 0
+    col = set()     # tracking the columns
+    neg_diagonal = set()    # (r - 1) tracking the negative diagonals
+    pos_diagonal = set()    # (r + 1)tracking the positive diagonals
 
-    # iterate thru rows
-    while r < n:
-        goback = False
-        # iterate thru columns
-        while c < n:
-            # check is current column is safe
-            safe = True
-            for cord in placed_queens:
-                col = cord[1]
-                if(col == c or col + (r-cord[0]) == c or
-                        col - (r-cord[0]) == c):
-                    safe = False
-                    break
+    board = [[] for num in range(num)]  # empty board
 
-            if not safe:
-                if c == n - 1:
-                    goback = True
-                    break
-                c += 1
+    def backtracking(r):
+        """recursive function for backtracking """
+        if r == num:    # getting to the last row
+            copy = board.copy()     # copy the current board
+            result.append(copy)
+            return
+
+        for c in range(num):    # validating used columns and diagonals
+            if c in col or (r - c) in neg_diagonal or (r + c) in pos_diagonal:
                 continue
 
-            # place queen
-            cords = [r, c]
-            placed_queens.append(cords)
-            # if last row, append solution and reset all to last unfinished row
-            # and last safe column in that row
-            if r == n - 1:
-                solutions.append(placed_queens[:])
-                for cord in placed_queens:
-                    if cord[1] < n - 1:
-                        r = cord[0]
-                        c = cord[1]
-                for i in range(n - r):
-                    placed_queens.pop()
-                if r == n - 1 and c == n - 1:
-                    placed_queens = []
-                    stop = True
-                r -= 1
-                c += 1
-            else:
-                c = 0
-            break
-        if stop:
-            break
-        # on fail: go back to previous row
-        # and continue from last safe column + 1
-        if goback:
-            r -= 1
-            while r >= 0:
-                c = placed_queens[r][1] + 1
-                del placed_queens[r]  # delete previous queen coordinates
-                if c < n:
-                    break
-                r -= 1
-            if r < 0:
-                break
-            continue
-        r += 1
+            # record found colums and diagonals
+            col.add(c)
+            neg_diagonal.add(r - c)
+            pos_diagonal.add(r + c)
+            board[r] = [r, c]
 
-    for idx, val in enumerate(solutions):
-        if idx == len(solutions) - 1:
-            print(val, end='')
-        else:
-            print(val)
+            # move to next row
+            backtracking(r + 1)
+
+            # undo movements
+            col.remove(c)
+            neg_diagonal.remove(r - c)
+            pos_diagonal.remove(r + c)
+
+            board[r] = []
+
+    backtracking(0)
+    return result
+
+
+if __name__ == '__main__':
+    boards = n_queens(n)
+    for board in boards:
+        print(board)
